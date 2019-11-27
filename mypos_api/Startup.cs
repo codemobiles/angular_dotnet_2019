ac7b0325d8806d80bb68397cac5cbb6a70f1f92e
iBlurBlur <tanakorn.ngam@gmail.com>
2019-11-27 13:29:02 +0700

    ..

--- mypos_api/Startup.cs
+++ mypos_api/Startup.cs
@@ -34,6 +34,64 @@ namespace mypos_api
             services.AddDbContext<DatabaseContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("ConnectionSQLServer")));
 
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "ToDo API",
+                });
+
+                c.SwaggerDoc("v2", new OpenApiInfo
+                {
+                    Version = "v2",
+                    Title = "POS API",
+                    Description = "A simple example ASP.NET Core Web API",
+                    TermsOfService = new Uri("http://codemobiles.com"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "iBlur Blur",
+                        Email = "codemobiles@gmail.com",
+                        Url = new Uri("http://codemobiles.com"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under MIT",
+                        Url = new Uri("http://codemobiles.com"),
+                    },
+                });
+
+                var securitySchema = new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                };
+                c.AddSecurityDefinition("Bearer", securitySchema);
+
+                var securityRequirement = new OpenApiSecurityRequirement();
+                securityRequirement.Add(securitySchema, new[] { "Bearer" });
+                c.AddSecurityRequirement(securityRequirement);
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetEntryAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+
+                // Uses full schema names to avoid v1/v2/v3 schema collisions
+                // see: https://github.com/domaindrivendev/Swashbuckle/issues/442
+                c.CustomSchemaIds(x => x.FullName);
+            });
+
+
+
             // Declare IProductRepo Service for DI
             services.AddScoped<IProductRepo, ProductRepo>();
         }
@@ -48,9 +106,33 @@ namespace mypos_api
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection(); 
+            app.UseHttpsRedirection();
 
-            app.UseRouting(); 
+            app.UseRouting();
+
+
+  
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            // http://localhost:<port>/swagger/<version-doc>/swagger.json
+            // mark: <version-doc> ref. c.SwaggerDoc("v1", ....)
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            // http://localhost:<port>/swagger
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
+                c.SwaggerEndpoint("/swagger/v2/swagger.json", "API V2");
+                c.DisplayOperationId();
+                c.DisplayRequestDuration();
+                // To serve the Swagger UI at the app's root 
+                //c.RoutePrefix = string.Empty;
+            });
+
+
+
+
 
             app.UseAuthorization();
 
