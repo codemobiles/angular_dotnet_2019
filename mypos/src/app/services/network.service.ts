d665108fea7042c26afdb07089d181a78a8a6b1c
iBlurBlur <tanakorn.ngam@gmail.com>
2019-11-28 13:32:59 +0700

    ..

--- mypos/src/app/services/network.service.ts
+++ mypos/src/app/services/network.service.ts
@@ -3,20 +3,32 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ProductResponse } from '../models/product.model';
 import { RegisterResponse, LoginResponse } from '../models/user.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NetworkService {
 
+  private headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+  private hostURL = environment.baseAPIURL;  
+  private apiURL = `${this.hostURL}`;
+  // -----------------------------------------------------
+  private loginURL = `${this.apiURL}/auth/login`;
+  private registerURL = `${this.apiURL}/auth/register`;
+  private productURL = `${this.apiURL}/product`;
+  public productImageURL = `${this.apiURL}/product/images`;
+  private outOfStockURL = `${this.productURL}/count/out_of_stock`;
+  private transactionURL = `${this.apiURL}/transaction`;
+  
   constructor(private httpClient: HttpClient) { }
 
   register(data): Observable<RegisterResponse> {
-    return this.httpClient.post<RegisterResponse>("http://192.168.0.102:5000/api/auth/register", data)
+    return this.httpClient.post<RegisterResponse>("xxxxxxapi/auth/register", data)
   }
 
   login(data): Observable<LoginResponse> {
-    return this.httpClient.post<LoginResponse>("http://192.168.0.102:5000/api/auth/login", data)
+    return this.httpClient.post<LoginResponse>("xxxxx/api/auth/login", data)
   }
 
   getAllProduct(): Observable<ProductResponse> {
